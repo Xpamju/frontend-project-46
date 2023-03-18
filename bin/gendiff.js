e15e9f777a5021475fc790354bf051c78e34f495
Julia Khramchenkova <Pineapple05955@gmail.com>
2023-03-18 18:41:34 +0300

    new opt

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,8 +3,15 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .name('gendiff')
+  .option('-f, --format <type>', 'output format')
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0');
+ 
+  
+
 
 program.parse();
+
