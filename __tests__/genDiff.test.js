ffd41be385e2e6eef646afdd151c27d1318a8d86
Julia Khramchenkova <Pineapple05955@gmail.com>
2023-04-24 23:06:12 +0300

    test recover

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,35 +1,18 @@
 
 // import path from 'path';
 import { readFileSync } from 'fs';
-import newobj from '/Users/igor/frontend-project-46/src/genDiff.js'
-import {obj} from '/Users/igor/frontend-project-46/src/genDiff.js'
+import {obj} from '../src/genDiff.js'
+import newobj from '../src/genDiff.js'
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
-// import { test, expect } from '@jest/globals';
-// import pkg from '@jest/globals';
-// const { test, expect } = pkg;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-// describe('newobj', () => {
-//     test('returns the correct object', () => {
-//       const data1 = JSON.parse(readFileSync(join(__dirname, 'file1.json'), 'utf-8'));
-//       const data2 = JSON.parse(readFileSync(join(__dirname, 'file2.json'), 'utf-8'));
-//       const expected = JSON.parse(readFileSync(join(dirname, 'expected.json'), 'utf-8'));
-//       const actual = newobj(data1, data2);
-//       expect(actual).toEqual(expected);
-//     });
-//   });
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
 
-// const test = readFile('file1.json')
-// console.log(test)
-
 test('returns the correct object', () => {
-
   const data1 = readFile('file1.json')
   const data2 = readFile('file2.json')
   console.log([].toString.call(data1))
@@ -37,6 +20,4 @@ test('returns the correct object', () => {
   const expected = readFile('expected.json')
   const actual = newobj(JSON.parse(data1), JSON.parse(data2))
   expect(actual).toEqual(expected)
-})
-
-// console.log(JSON.parse(readFileSync(path.join(__dirname, '__fixtures__', 'file1.json', '../'))))
+});
