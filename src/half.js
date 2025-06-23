774ae5e0c4f160da137f49a477875ce738a310d4
Julia <pineapple05955@gmail.com>
2025-06-23 18:09:22 +0300

    6s

--- src/half.js
+++ src/half.js
@@ -1,47 +1,56 @@
 import _ from 'lodash';
 
-const buildDiff = (data1, data2, depth = 1) => {
-  const keys = _.union(Object.keys(data1), Object.keys(data2));
+const buildDiff = (data1, data2) => {
+  const keys = _.union(_.keys(data1), _.keys(data2)).sort();
   
-  return keys.sort().map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
-
+  return keys.map((key) => {
     if (!_.has(data2, key)) {
-      return { key, type: 'removed', value: value1, depth };
-    }
-    if (!_.has(data1, key)) {
-      return { key, type: 'added', value: value2, depth };
-    }
-
-    if (_.isEqual(value1, value2)) {
-      return { key, type: 'unchanged', value: value1, depth };
-    }
-
-    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return { 
-        key, 
-        type: 'nested', 
-        children: buildDiff(value1, value2, depth + 1),
-        depth 
+      return {
+        key,
+        type: 'removed',
+        value: data1[key]
       };
     }
-
-    return { 
-      key, 
-      type: 'changed', 
-      oldValue: value1, 
-      newValue: value2,
-      depth 
+    
+    if (!_.has(data1, key)) {
+      return {
+        key,
+        type: 'added', 
+        value: data2[key]
+      };
+    }
+    
+    const value1 = data1[key];
+    const value2 = data2[key];
+    
+    if (_.isEqual(value1, value2)) {
+      return {
+        key,
+        type: 'unchanged',
+        value: value1
+      };
+    }
+    
+    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      return {
+        key,
+        type: 'nested',
+        children: buildDiff(value1, value2)
+      };
+    }
+    
+    return {
+      key,
+      type: 'changed',
+      oldValue: value1,
+      newValue: value2
     };
   });
 };
 
-const compare = (data1, data2) => {
-  return {
-    type: 'root',
-    children: buildDiff(data1, data2),
-  };
-};
+const compare = (data1, data2) => ({
+  type: 'root',
+  children: buildDiff(data1, data2)
+});
 
 export default compare;
